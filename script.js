74e90ca591d683cf98dc9729c92b7c95a45084e5
Biancalima22 <silva.bianca22@escola.pr.gov.br>
2025-08-05 12:38:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const BotaoMostraPalavras = document. querySelector('#botao-palavrachave');
 
 BotaoMostraPalavras.addEventListener('clik,mostraPalavrasChaves');
+
+function mostraPalavrasChaves() {
+    const texto = document .querySelector ('#entrada-de-texto') . value;
+    const campoResultado = document.querySelector ('#resultado-pralavrachave');
+     const palavras = texto.split('')
+
+ campoResultado. textContent = texto;
+}
